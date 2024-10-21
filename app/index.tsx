af0f062eaf6c60b4f02d3e1008055820699f8567
chamidugihan <w.g.c.g.ellepola@gmail.com>
2024-10-21 21:00:24 +0530

    button test

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,17 @@
 import { FlatList, Text, View } from "react-native";
 import products from "../assets/producr.json";
 import ProductListItem from "../components/ProductListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 
 export default function HomeScreen() {
     return (
-        <FlatList data={products} renderItem={
-            ({ item }) =>
-                <ProductListItem  product={item}/>
-        } />
+        // <FlatList data={products} renderItem={
+        //     ({ item }) =>
+        //         <ProductListItem  product={item}/>
+        // } />
+        <Button>
+            <ButtonText>Press me</ButtonText>
+        </Button>
     );
 }
